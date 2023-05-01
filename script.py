221de3210f6d804c06cae5cc81572a34ed022489
dgomez <xd43vild69@gmail.com>
2023-05-01 18:18:30 -0500

    script for test python features

---
+++ script.py
@@ -0,0 +1,23 @@
+print("Goodbye, World!")
+
+# Comment python
+
+def myFunction():
+    for x in range(3, 8, 3):
+        print(x)    
+
+def myFunction2():
+    mylist = []
+
+    mylist.append(1)
+    mylist.append(2)
+    mylist.append(3)
+    print(mylist[0]) # prints 1
+    print(mylist[1]) # prints 2
+    print(mylist[2]) # prints 3
+
+    # prints out 1,2,3
+    for x in mylist:
+        print(x)
+
+myFunction()        
