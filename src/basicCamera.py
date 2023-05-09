406b705f82d150b4823b4c4b5e8cdda5535352cc
xd43vild69 <xd43vild69@gmail.com>
2023-05-08 21:00:59 -0500

    Add main camera

---
+++ src/basicCamera.py
@@ -0,0 +1,5 @@
+import bpy
+ 
+camera_data = bpy.data.cameras.new(name='camera_main')
+camera_object = bpy.data.objects.new('camera_main', camera_data)
+bpy.context.scene.collection.objects.link(camera_object)
