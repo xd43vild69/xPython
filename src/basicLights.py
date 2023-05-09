5f5d003606476b4fd9c4332d2b3ddeb2e016055c
xd43vild69 <xd43vild69@gmail.com>
2023-05-08 20:58:27 -0500

    Add main light to scene

---
+++ src/basicLights.py
@@ -0,0 +1,21 @@
+import bpy
+
+# create light datablock, set attributes
+light_data = bpy.data.lights.new(name="light_main", type='POINT')
+light_data.energy = 30
+
+# create new object with our light datablock
+light_object = bpy.data.objects.new(name="light_main", object_data=light_data)
+
+# link light object
+bpy.context.collection.objects.link(light_object)
+
+# make it active 
+bpy.context.view_layer.objects.active = light_object
+
+#change location
+light_object.location = (0, 0, 5)
+
+# update scene, if needed
+dg = bpy.context.evaluated_depsgraph_get() 
+dg.update()
